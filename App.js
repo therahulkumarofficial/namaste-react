da16fc13772281fd7b0c6d8e981f3d3797d4d19c
therahulkumarofficial <rahulkumarpersonal01@gmail.com>
2024-05-12 12:06:48 +0530

    'OK'

--- App.js
+++ App.js
@@ -1,28 +1,3 @@
-/**
- *
- *
- *
- *
- *
- * <div class="parent">
- *    <div class="child">
- *    <h1>I'am h1 tag</h1>
- *    <h2>I'am h2 tag</h2>
- *    </div>
- *    <div class="child2">
- *    <h1>I'am h1 tag</h1>
- *    <h2>I'am h2 tag</h2>
- *    </div>
- * </div>
- *
- *  ReactElement(object)  ⟹  HTML (Browser Understands)
- *
- *
- *
- *
- *
- */
-
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I'am h1 tag"),
@@ -33,19 +8,13 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {title: "h2 tag"}, "I'am h2 tag"),
   ]),
 ]);
-console.log(parent);
 const subroot = ReactDOM.createRoot(document.getElementById("subroot"));
 subroot.render(parent);
 
-//JSX
-
-// The code you provided is a valid way to create a React component and render it to the DOM
 const heading = React.createElement(
   "h1",
   { id: "heading", title: "Hello" },
   "Hello World by React!"
 );
-console.log(heading); //object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading); // converting object to h1 tag
-//everything inside root is replace by root.render(heading)
+root.render(heading);
